5102ae5d7c0b2d5812a8d03004719e911be29613
Manikandan T S <manikandan.citeee@gmail.com>
2013-06-12 22:51:29 +0530

    first commit

---
+++ src/com/schneider/i3d/MainActivity.java
@@ -0,0 +1,92 @@
+package com.schneider.i3d;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher.ViewFactory;
+
+public class MainActivity extends Activity implements ViewFactory{
+	protected static final String MESSAGE = "com.schneider.message";
+    private ImageSwitcher mSwitcher;
+	private int i=0;
+	protected Intent intent;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_home);
+		mSwitcher = (ImageSwitcher) findViewById(R.id.latestUpdate);
+        mSwitcher.setFactory(this);
+        mSwitcher.setInAnimation(AnimationUtils.loadAnimation(this,android.R.anim.fade_in));
+        mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,android.R.anim.fade_out));
+        mSwitcher.setBackgroundColor(Color.WHITE);
+        final Handler mHandler = new Handler();
+        final Runnable mUpdateResults = new Runnable() {
+            public void run() {
+     	        mSwitcher.setImageResource(mImageIds[++i%mImageIds.length]);
+             }
+        };
+         int delay = 200;
+         int period = mImageIds.length*delay;
+         Timer timer = new Timer();
+         timer.scheduleAtFixedRate(new TimerTask() {
+ 
+        public void run() {
+              mHandler.post(mUpdateResults);
+         }
+      }, delay, period);
+         ImageButton bScan = (ImageButton) findViewById(R.id.ibScan);
+         ImageButton bSearch = (ImageButton) findViewById(R.id.ibSearch);
+         final EditText eSearch = (EditText) findViewById(R.id.ieQuery);
+         bScan.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+ 				/*intent = new Intent(MainActivity.this,ScanSelectorActivity.class);
+ 				startActivity(intent);*/
+ 			}
+ 		});
+         bSearch.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				SearchString = eSearch.getText().toString();
+				/*intent = new Intent(MainActivity.this,OptionsActivity.class);
+	    		intent.putExtra(MESSAGE, SearchString);
+				startActivity(intent);*/
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	 private Integer[] mImageIds = { R.drawable.anya, R.drawable.asfora,
+	            R.drawable.merten, R.drawable.ovalis, R.drawable.sedna,
+	            R.drawable.ultimate, R.drawable.unica, R.drawable.vivace,
+	            R.drawable.zencelo};
+		private String SearchString;
+
+		public View makeView() {
+			ImageView i = new ImageView(this);
+	        i.setBackgroundColor(0xFFFFFFFF);
+	        i.setScaleType(ImageView.ScaleType.FIT_CENTER);
+	        i.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	        return i;
+		}
+}
